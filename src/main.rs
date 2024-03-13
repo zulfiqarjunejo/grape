a67b567fe71fc01a0dfd5c26757a6fd69169d06e
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2024-03-13 23:42:56 +0100

    feat: implement search

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use grape::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = grape::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    };
 }
